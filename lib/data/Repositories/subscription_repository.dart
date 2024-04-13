3c0c3f981b5e95a4f611901e32f36c661fb02ac8
pravin1217 <pravin17.1997@gmail.com>
2024-04-13 21:07:07 +0530

    first commit

---
+++ lib/data/Repositories/subscription_repository.dart
@@ -0,0 +1,35 @@
+import '../../utils/api.dart';
+import '../../utils/hive_utils.dart';
+import '../model/data_output.dart';
+import '../model/subscription_pacakage_model.dart';
+
+class SubscriptionRepository {
+  Future<DataOutput<SubscriptionPackageModel>> getSubscriptionPacakges() async {
+    Map<String, dynamic> response =
+        await Api.get(url: Api.getPackage, queryParameters: {});
+
+    List<SubscriptionPackageModel> modelList = (response['data'] as List)
+        .map((element) => SubscriptionPackageModel.fromJson(element))
+        .toList();
+
+    return DataOutput(total: modelList.length, modelList: modelList);
+  }
+
+  Future<void> subscribeToPackage(
+      int packageId, bool isPackageAvailable) async {
+    try {
+      Map<String, dynamic> parameters = {
+        Api.packageId: packageId,
+        Api.userid: HiveUtils.getUserId(),
+        if (isPackageAvailable) 'flag': 1,
+      };
+      // if (isPackageAvailable) {
+      //   parameters['flag'] = 1;
+      // }
+
+      await Api.post(url: Api.userPurchasePackage, parameter: parameters);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
